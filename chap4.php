86288958b02f2a14105c9e89bddd074326691f84
k.max <nike.taka@gmail.com>
2020-05-15 11:36:22 +0900

    ペット課題完成

--- chap4.php
+++ chap4.php
@@ -1,3 +1,12 @@
 <?php
-
+require_once __DIR__ . "/chap4_pet.php";
+$owners = [
+    ["name" => "斎藤", "animal" => "猫", "cnt" => 3],
+    ["name" => "鈴木", "animal" => "柴犬", "cnt" => 1],
+    ["name" => "藤井", "animal" => ["猫", "チワワ", "うさぎ"], "cnt" => 3]
+];
+foreach ($owners as $owner) {
+    $animal = getPet($animal);
+    createMsg($owner["name"], $animal, $owner["cnt"]);
+}
 ?>
